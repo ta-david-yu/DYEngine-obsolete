bc3d0dea82a26e0f91cdf382f4397bf3323d9525
kamisama366095 <d366095@gmail.com>
2017-09-21 10:19:26 +0800

    Track header file

---
+++ include/DYEngine/Scene.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <DYEngine\Main.h>
+
+#include <vector>
+#include <string>
+#include <memory>
+
+namespace DYE
+{
+	typedef unsigned int SceneID;
+	//====================================================================================
+	//	Scene: managing objects
+	//====================================================================================
+	class Scene
+	{
+		typedef std::pair<InstanceID, std::unique_ptr<Entity>> EntityListPair;
+		typedef std::vector<EntityListPair> EntityList;
+
+		//==========================================
+		//	memeber/variable
+		//==========================================
+	private:
+		SceneID m_SceneID;
+		std::string m_SceneName;
+		EntityList m_EntityList;
+		//==========================================
+		//	flag
+		//==========================================
+
+		//==========================================
+		//	procedure
+		//==========================================
+
+		//==========================================
+		//	method
+		//==========================================
+		Entity* CreateEntity();
+		Entity* CreateEntity(const std::string& _name);
+		//==========================================
+		//	getter
+		//==========================================
+	public:
+		SceneID GetSceneID() const;
+		std::string GetSceneName() const;
+		//==========================================
+		//	setter
+		//==========================================
+
+		//==========================================
+		//	constructor/destructor
+		//==========================================
+		Scene();
+		~Scene();
+	};
+}
